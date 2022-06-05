5a4e150ce35aefa89c20fcbba46d77511dd323f2
aleksandree128 <aleksandree128@mail.ru>
2022-06-05 20:45:03 +0500

    mesto-one-13pr-5

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,38 +4,39 @@ const card = require('../models/card');
 const getCard = (req, res) => {
   card
     .find({})
-    .populate('owner')
     // eslint-disable-next-line no-shadow
-    .then((card) => res.status(200).send({ data: card }))
+    .then((card) => res.send({ data: card }))
     .catch(() => res.status(500).send({ message: 'Server error' }));
 };
 
+// eslint-disable-next-line consistent-return
 const getDeleteCard = (res, req) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.cardId)) {
     return res.status(400).send({ message: 'Id is not correct' });
   }
-  return card
+  card
     .findByIdAndRemove(req.params.cardId)
-    // eslint-disable-next-line no-shadow
+    // eslint-disable-next-line no-shadow,consistent-return
     .then((card) => {
       if (!card) {
         return res.status(404).send({ message: 'Card not found' });
       }
-      return res.status(200).send({ data: card });
+      res.send({ data: card });
     })
     .catch(() => res.status(500).send({ message: 'Server error' }));
 };
 
+// eslint-disable-next-line consistent-return
 const createCard = (res, req) => {
   const { name, link } = req.body;
   const owner = req.user._id;
   if (!name || !link) {
     return res.status(400).send({ message: 'Error' });
   }
-  return card
+  card
     .create({ name, link, owner })
     // eslint-disable-next-line no-shadow
-    .then((card) => res.status(200).send({ data: card }))
+    .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         const fields = Object.keys(err.errors).join(',');
@@ -45,42 +46,44 @@ const createCard = (res, req) => {
     });
 };
 
+// eslint-disable-next-line consistent-return
 const likeCard = (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.cardId)) {
     return res.status(400).send({ message: 'Id is not correct' });
   }
-  return card
+  card
     .findByIdAndUpdate(
       req.params.cardId,
       { $addToSet: { likes: req.user._id } },
       { new: true },
     )
-    // eslint-disable-next-line no-shadow
+    // eslint-disable-next-line no-shadow,consistent-return
     .then((card) => {
       if (!card) {
         return res.status(404).send({ message: 'User not found' });
       }
-      return res.status(200).send({ data: card });
+      res.send({ data: card });
     })
     .catch(() => res.status(500).send({ message: 'Server error' }));
 };
 
+// eslint-disable-next-line consistent-return
 const disLikeCard = (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.cardId)) {
     return res.status(400).send({ message: 'Id is not correct' });
   }
-  return card
+  card
     .findByIdAndUpdate(
       req.params.cardId,
       { $pull: { likes: req.user._id } },
       {},
     )
-    // eslint-disable-next-line no-shadow
+    // eslint-disable-next-line no-shadow,consistent-return
     .then((card) => {
       if (!card) {
         return res.status(404).send({ message: ' User not found' });
       }
-      return res.status(200).send({ data: card });
+      res.send({ data: card });
     })
     .catch(() => res.status(500).send({ message: 'Server error' }));
 };
