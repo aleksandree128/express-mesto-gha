a88e8b115a1941692cc8f1bd125e754f67690bc0
aleksandree128 <57660367+aleksandree128@users.noreply.github.com>
2022-07-12 19:48:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,12 +9,33 @@ const NotFoundErrors = require('./code_errors/notFound-errors');
 const app = express();
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.use('*', cors({
   origin: 'https://mesto-korshinov.nomoredomains.xyz',
   credentials: true,
 }));
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger);
+
+app.use(express.json());
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.use(errorLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
